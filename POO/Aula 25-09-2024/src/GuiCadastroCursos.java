59137662d5fc133e65b0f03adc940077033d1490
Rayssa Mara Caldini Costa <rayssa_mcc@hotmail.com>
2024-10-09 16:01:16 -0300

    inferno

--- POO/Aula 25-09-2024/src/GuiCadastroCursos.java
+++ POO/Aula 25-09-2024/src/GuiCadastroCursos.java
@@ -4,7 +4,8 @@ import java.awt.event.*;
 public class GuiCadastroCursos extends JPanel {
     JLabel label1, label2, label3, label4, label5, label6;
     JButton btGravar, btAlterar, btExcluir, btNovo, btLocalizar, btCancelar, btSair;
-    static JTextField tfIdCurso, tfNome, tfCarga, tfTipo, tfModalidade, tfCodigo;
+    static JTextField tfIdCurso, tfNome, tfCarga;
+    JComboBox<String> cbTipo, cbModalidade, cbCodigo;
     private CursosDAO cursos;
 
     public GuiCadastroCursos() {
@@ -14,7 +15,7 @@ public class GuiCadastroCursos extends JPanel {
 
     public void inicializarComponentes() {
         setLayout(null);
-        
+    
         // Labels
         label1 = new JLabel("Id do Curso:");
         label1.setBounds(10, 10, 100, 20);
@@ -22,13 +23,13 @@ public class GuiCadastroCursos extends JPanel {
         label2.setBounds(10, 40, 100, 20);
         label3 = new JLabel("Carga Horária:");
         label3.setBounds(10, 70, 100, 20);
-        label4 = new JLabel("Tipo:");
+        label4 = new JLabel("Tipo do Curso:");
         label4.setBounds(10, 100, 100, 20);
         label5 = new JLabel("Modalidade:");
         label5.setBounds(10, 130, 100, 20);
-        label6 = new JLabel("Código:");
+        label6 = new JLabel("Código do Depto:");
         label6.setBounds(10, 160, 100, 20);
-
+    
         // TextFields
         tfIdCurso = new JTextField(8);
         tfIdCurso.setBounds(120, 10, 100, 20);
@@ -36,42 +37,44 @@ public class GuiCadastroCursos extends JPanel {
         tfNome.setBounds(120, 40, 300, 20);
         tfCarga = new JTextField(10);
         tfCarga.setBounds(120, 70, 100, 20);
-        tfTipo = new JTextField(50);
-        tfTipo.setBounds(120, 100, 300, 20);
-        tfModalidade = new JTextField(50);
-        tfModalidade.setBounds(120, 130, 300, 20);
-        tfCodigo = new JTextField(50);
-        tfCodigo.setBounds(120, 160, 300, 20);
-
+    
+        // ComboBoxes
+        cbTipo = new JComboBox<>(new String[]{"Graduação", "Pós-Graduação", "Extensão"});
+        cbTipo.setBounds(120, 100, 300, 20);
+        cbModalidade = new JComboBox<>(new String[]{"Presencial", "EAD", "Presencial/EAD"});
+        cbModalidade.setBounds(120, 130, 300, 20);
+        cbCodigo = new JComboBox<>(new String[]{"1", "2"});
+        cbCodigo.setBounds(120, 160, 300, 20);
+    
         // Botões
         btGravar = new JButton("Gravar");
         btGravar.setToolTipText("Gravar");
         btGravar.setBounds(10, 200, 100, 20);
-
+    
         btAlterar = new JButton("Alterar");
         btAlterar.setToolTipText("Alterar");
         btAlterar.setBounds(120, 200, 100, 20);
-
+    
         btExcluir = new JButton("Excluir");
         btExcluir.setToolTipText("Excluir");
         btExcluir.setBounds(230, 200, 100, 20);
-
+    
         btLocalizar = new JButton("Localizar");
         btLocalizar.setToolTipText("Localizar");
         btLocalizar.setBounds(340, 200, 100, 20);
-
+    
         btNovo = new JButton("Novo");
         btNovo.setToolTipText("Novo");
         btNovo.setBounds(450, 200, 100, 20);
-
+    
         btCancelar = new JButton("Cancelar");
         btCancelar.setToolTipText("Cancelar");
         btCancelar.setBounds(560, 200, 100, 20);
-
+    
         btSair = new JButton("Sair");
         btSair.setToolTipText("Sair");
         btSair.setBounds(670, 200, 100, 20);
-
+    
         // Adicionando componentes
         add(label1);
         add(tfIdCurso);
@@ -80,11 +83,11 @@ public class GuiCadastroCursos extends JPanel {
         add(label3);
         add(tfCarga);
         add(label4);
-        add(tfTipo);
+        add(cbTipo);
         add(label5);
-        add(tfModalidade);
+        add(cbModalidade);
         add(label6);
-        add(tfCodigo);
+        add(cbCodigo);
         add(btNovo);
         add(btLocalizar);
         add(btGravar);
@@ -92,7 +95,7 @@ public class GuiCadastroCursos extends JPanel {
         add(btExcluir);
         add(btCancelar);
         add(btSair);
-
+    
         setBotoes(true, true, false, false, false, false);
         cursos = new CursosDAO();
         if (!cursos.bd.getConnection()) {
@@ -108,20 +111,20 @@ public class GuiCadastroCursos extends JPanel {
                 System.exit(0);
             }
         });
-
+    
         btNovo.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 limparCampos();
                 setBotoes(false, false, true, false, false, true);
             }
         });
-
+    
         btCancelar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 limparCampos();
             }
         });
-
+    
         btGravar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (validarCampos()) {
@@ -130,10 +133,10 @@ public class GuiCadastroCursos extends JPanel {
                         cursos.curso.setIdCurso(idCurso);
                         cursos.curso.setNomeCurso(tfNome.getText());
                         cursos.curso.setCargaHoraria(Integer.parseInt(tfCarga.getText()));
-                        cursos.curso.setTipoCurso(tfTipo.getText());
-                        cursos.curso.setModalidadeCurso(tfModalidade.getText());
-                        cursos.curso.setCodigoCurso(Integer.parseInt(tfCodigo.getText()));
-
+                        cursos.curso.setTipoCurso((String) cbTipo.getSelectedItem());
+                        cursos.curso.setModalidadeCurso((String) cbModalidade.getSelectedItem());
+                        cursos.curso.setCodigoCurso(Integer.parseInt((String) cbCodigo.getSelectedItem())); // Corrigido para converter para int
+        
                         JOptionPane.showMessageDialog(null, cursos.atualizar(CursosDAO.INCLUSAO));
                         limparCampos();
                     } catch (NumberFormatException ex) {
@@ -142,7 +145,7 @@ public class GuiCadastroCursos extends JPanel {
                 }
             }
         });
-
+    
         btAlterar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (validarCampos()) {
@@ -151,9 +154,9 @@ public class GuiCadastroCursos extends JPanel {
                         cursos.curso.setIdCurso(idCurso);
                         cursos.curso.setNomeCurso(tfNome.getText());
                         cursos.curso.setCargaHoraria(Integer.parseInt(tfCarga.getText()));
-                        cursos.curso.setTipoCurso(tfTipo.getText());
-                        cursos.curso.setModalidadeCurso(tfModalidade.getText());
-
+                        cursos.curso.setTipoCurso((String) cbTipo.getSelectedItem());
+                        cursos.curso.setModalidadeCurso((String) cbModalidade.getSelectedItem());
+    
                         JOptionPane.showMessageDialog(null, cursos.atualizar(CursosDAO.ALTERACAO));
                         limparCampos();
                     } catch (NumberFormatException ex) {
@@ -162,7 +165,7 @@ public class GuiCadastroCursos extends JPanel {
                 }
             }
         });
-
+    
         btExcluir.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (tfIdCurso.getText().isEmpty()) {
@@ -170,7 +173,7 @@ public class GuiCadastroCursos extends JPanel {
                     tfIdCurso.requestFocus();
                     return;
                 }
-
+    
                 try {
                     int idCurso = Integer.parseInt(tfIdCurso.getText());
                     cursos.curso.setIdCurso(idCurso);
@@ -179,7 +182,7 @@ public class GuiCadastroCursos extends JPanel {
                     tfIdCurso.requestFocus();
                     return;
                 }
-
+    
                 cursos.localizar();
                 int n = JOptionPane.showConfirmDialog(null, cursos.curso.getNomeCurso(), "Excluir o curso?", JOptionPane.YES_NO_OPTION);
                 if (n == JOptionPane.YES_OPTION) {
@@ -188,14 +191,14 @@ public class GuiCadastroCursos extends JPanel {
                 }
             }
         });
-
+    
         btLocalizar.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 atualizarCampos();
             }
         });
     }
-
+    
     public boolean validarCampos() {
         if (tfIdCurso.getText().isEmpty()) {
             JOptionPane.showMessageDialog(null, "O campo Id do curso não pode ser vazio!");
@@ -212,25 +215,25 @@ public class GuiCadastroCursos extends JPanel {
             tfCarga.requestFocus();
             return false;
         }
-        if (tfModalidade.getText().isEmpty()) {
+        if (cbModalidade.getSelectedItem() == null) {
             JOptionPane.showMessageDialog(null, "O campo Modalidade não pode ser vazio!");
-            tfModalidade.requestFocus();
+            cbModalidade.requestFocus();
             return false;
         }
         return true;
     }
-
+    
     public void limparCampos() {
         tfIdCurso.setText("");
         tfNome.setText("");
         tfCarga.setText("");
-        tfTipo.setText("");
-        tfModalidade.setText("");
-        tfCodigo.setText("");
+        cbTipo.setSelectedIndex(-1);
+        cbModalidade.setSelectedIndex(-1);
+        cbCodigo.setSelectedIndex(-1);
         tfIdCurso.requestFocus();
         setBotoes(true, true, false, false, false, false);
     }
-
+    
     public void atualizarCampos() {
         try {
             int idCurso = Integer.parseInt(tfIdCurso.getText());
@@ -240,13 +243,13 @@ public class GuiCadastroCursos extends JPanel {
             tfIdCurso.requestFocus();
             return;
         }
-
+    
         if (cursos.localizar()) {
             tfNome.setText(cursos.curso.getNomeCurso());
             tfCarga.setText(String.valueOf(cursos.curso.getCargaHoraria()));
-            tfTipo.setText(cursos.curso.getTipoCurso());
-            tfModalidade.setText(cursos.curso.getModalidadeCurso());
-            tfCodigo.setText(String.valueOf(cursos.curso.getCodigoCurso()));
+            cbTipo.setSelectedItem(cursos.curso.getTipoCurso());
+            cbModalidade.setSelectedItem(cursos.curso.getModalidadeCurso());
+            cbCodigo.setSelectedItem(cursos.curso.getCodigoCurso());
             setBotoes(true, true, false, true, true, true);
         } else {
             JOptionPane.showMessageDialog(null, "Curso não encontrado!");
